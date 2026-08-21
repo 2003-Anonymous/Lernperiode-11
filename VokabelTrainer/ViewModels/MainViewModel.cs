34837b0cd7b1944736d5fda2dbf9c7a3277341ba
Anonymous <j.sommerhalder.inf24@stud.bbbaden.ch>
2026-08-21 06:29:48 +0200

    Added a short example wordlist

--- VokabelTrainer/ViewModels/MainViewModel.cs
+++ VokabelTrainer/ViewModels/MainViewModel.cs
@@ -1,3 +1,4 @@
+using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace VokabelTrainer.ViewModels;
@@ -6,4 +7,13 @@ public partial class MainViewModel : ViewModelBase
 {
     [ObservableProperty]
     public partial string Greeting { get; set; } = "Welcome to Avalonia!";
+
+    public ObservableCollection<string> Woerter { get; } =
+    [
+        "Apfel: jablko",
+        "Brot: chleb",
+        "Katze: kot",
+        "Tiger: tygrys",
+        "Bieber: bober"
+    ];
 }
